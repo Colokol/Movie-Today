d2c75b2eb6e53f84694a7023dbcceb4de01e1183
Colokol <Colokol1@gmail.com>
2023-12-24 17:50:22 +0300

    File Struct project

---
+++ Movie Today/Models/FilmModel.swift
@@ -0,0 +1,8 @@
+//
+//  FilmModel.swift
+//  Movie Today
+//
+//  Created by Uladzislau Yatskevich on 24.12.23.
+//
+
+import Foundation
---
+++ Movie Today/Network Layer/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Movie Today
+//
+//  Created by Uladzislau Yatskevich on 24.12.23.
+//
+
+import Foundation
