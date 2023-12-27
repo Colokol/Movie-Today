eb7a2af0f5d6416129a436a94f65d2fdfe650bd5
AnnaZaitsava <anya.polden@gmail.com>
2023-12-27 03:42:50 +0300

    - Wishlist UI

--- Movie Today/Extension/UIColor + Extension.swift
+++ Movie Today/Extension/UIColor + Extension.swift
@@ -10,4 +10,11 @@ extension UIColor {
     static var background: UIColor {
         return UIColor(red: 31/255, green: 29/255, blue: 43/255, alpha: 1.0)
     }
+    
+    static var whiteGray: UIColor {
+        return UIColor(red: 235/255, green: 235/255, blue: 239/255, alpha: 1.0)
+    }
+    static var soft: UIColor {
+        return UIColor(red: 37/255, green: 40/255, blue: 54/255, alpha: 1.0)
+    }
 }
--- Movie Today/Extension/UILabel + Extension.swift
+++ Movie Today/Extension/UILabel + Extension.swift
@@ -5,4 +5,15 @@
 //  Created by Uladzislau Yatskevich on 24.12.23.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    convenience init(text: String = "", font: UIFont? = .montserratRegular(ofSize: 14), textColor: UIColor, textAlignment: NSTextAlignment = .left, numberOfLines: Int = 1) {
+        self.init()
+        self.text = text
+        self.font = font
+        self.textColor = textColor
+        self.textAlignment = textAlignment
+        self.numberOfLines = numberOfLines
+    }
+}
