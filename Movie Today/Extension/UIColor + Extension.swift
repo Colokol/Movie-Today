f1114d31237abcb65cbbc2221aa3c58142abf0f0
AnnaZaitsava <anya.polden@gmail.com>
2023-12-28 03:20:24 +0300

    - added additional colors

--- Movie Today/Extension/UIColor + Extension.swift
+++ Movie Today/Extension/UIColor + Extension.swift
@@ -17,4 +17,38 @@ extension UIColor {
     static var soft: UIColor {
         return UIColor(red: 37/255, green: 40/255, blue: 54/255, alpha: 1.0)
     }
+    
+    static var dark: UIColor {
+        return UIColor(red: 31/255, green: 29/255, blue: 43/255, alpha: 1.0)
+    }
+    
+    static var blueAccent: UIColor {
+        return UIColor(red: 18/255, green: 205/255, blue: 217/255, alpha: 1.0)
+    }
+    
+    static var customGreen: UIColor {
+        return UIColor(red: 34/255, green: 176/255, blue: 125/255, alpha: 1.0)
+    }
+    
+    static var customOrange: UIColor {
+        return UIColor(red: 255/255, green: 135/255, blue: 0/255, alpha: 1.0)
+    }
+    
+    static var customRed: UIColor {
+        return UIColor(red: 251/255, green: 65/255, blue: 65/255, alpha: 1.0)
+    }
+    
+    static var customGray: UIColor {
+        return UIColor(red: 146/255, green: 146/255, blue: 157/255, alpha: 1.0)
+    }
+    
+    static var customDarkGray: UIColor {
+        return UIColor(red: 105/255, green: 105/255, blue: 116/255, alpha: 1.0)
+    }
+    
+    static var lineDark: UIColor {
+        return UIColor(red: 234/255, green: 234/255, blue: 234/255, alpha: 1.0)
+    }
 }
+
+
