cf08f2d088210366969581fd4da182b30ab7818e
Uladzislau Yatskevich <123534574+Colokol@users.noreply.github.com>
2023-12-29 23:21:34 +0300

    Profile and Search ViewController (#33)

--- Movie Today/Modules/HomeScreen/SectionsConfig/Sections.swift
+++ Movie Today/Modules/HomeScreen/SectionsConfig/Sections.swift
@@ -1,9 +1,9 @@
-//
-//  Sections.swift
-//  Movie Today
-//
-//  Created by macbook on 24.12.2023.
-//
+    //
+    //  Sections.swift
+    //  Movie Today
+    //
+    //  Created by macbook on 24.12.2023.
+    //
 
 import Foundation
 
@@ -11,25 +11,39 @@ enum Sections: Int, CustomStringConvertible, CaseIterable {
     case compilation, categories, mostPopular
     var description: String {
         switch self {
-        case .compilation:
-            return "Compilation"
-        case .categories:
-            return "Categories"
-        case .mostPopular:
-            return "Most Popular"
+            case .compilation:
+                return "Compilation"
+            case .categories:
+                return "Categories"
+            case .mostPopular:
+                return "Most Popular"
         }
     }
 }
 
 enum MovieListSections: Int, CustomStringConvertible, CaseIterable {
     case categories, popular
-    
+
     var description: String {
         switch self {
-        case .categories:
-            return "Categories"
-        case .popular:
-            return "Most Popular"
+            case .categories:
+                return "Categories"
+            case .popular:
+                return "Most Popular"
+        }
+    }
+}
+
+enum SectionsSearch: Int, CustomStringConvertible, CaseIterable {
+    case categories, compilation, mostPopular
+    var description: String {
+        switch self {
+            case .categories:
+                return "Categories"
+            case .compilation:
+                return "Compilation"
+            case .mostPopular:
+                return "Most Popular"
         }
     }
 }
