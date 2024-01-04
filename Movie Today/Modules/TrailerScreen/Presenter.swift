e7da37454d495a25740e9db7d82156e75af395c1
macbook <yakovenko06122002@icloud.com>
2024-01-04 15:16:28 +0300

    The trailerController is ready

--- Movie Today/Modules/TrailerScreen/Presenter.swift
+++ Movie Today/Modules/TrailerScreen/Presenter.swift
@@ -9,13 +9,16 @@ import Foundation
 
 protocol TrailerViewProtocol: AnyObject {
     func update(with model: Doc, text: String)
+    func updateActors()
 }
 
 protocol TrailerPresenterProtocol: AnyObject {
     init(view: TrailerViewProtocol, model: Doc, text: String)
     var model: Doc? { get set }
     var videoID: String? { get set }
+    var filmPersons: [Person]? { get set }
     func config()
+    func getActors()
 }
 
 final class TrailerPresenter: TrailerPresenterProtocol {
@@ -23,10 +26,30 @@ final class TrailerPresenter: TrailerPresenterProtocol {
     weak var view: TrailerViewProtocol?
     var model: Doc?
     var videoID: String?
+    var filmPersons: [Person]?
+    let networkManager = NetworkManager()
     
     func config() {
         if let model = model, let id = videoID {
             self.view?.update(with: model, text: id)
+        }
+    }
+    
+    func getActors() {
+        guard let model else { return }
+        networkManager.getPersonForMovie(id: model.id) { result in
+            switch result {
+            case .success(let actor):
+                if self.filmPersons == nil {
+                    self.filmPersons = [Person]()
+                }
+                self.filmPersons?.append(contentsOf: actor.docs)
+                DispatchQueue.main.async {
+                    self.view?.updateActors()
+                }
+            case .failure(let error):
+                print(error.localizedDescription, "TrailerPresenterError")
+            }
         }
     }
     
--- Movie Today/Modules/TrailerScreen/TrailerController.swift
+++ Movie Today/Modules/TrailerScreen/TrailerController.swift
@@ -38,26 +38,62 @@ final class TrailerController: UIViewController {
         return stack
     }()
     
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "chevron.backward"), for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .soft
+        button.frame = .init(x: 0, y: 0, width: 24, height: 24)
+        button.layer.cornerRadius = button.bounds.size.width / 2
+        return button
+    }()
+    private let likeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "heart"), for: .normal)
+        return button
+    }()
+    
+    private let header: UILabel = {
+        let label = UILabel()
+        label.text = "Cast and Crew"
+        label.font = .montserratSemiBold(ofSize: 16)
+        label.textColor = .white
+        return label
+    }()
+    
     private var collectionView: UICollectionView!
     private let videoID = "F478PvRt74Y"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.hidesBottomBarWhenPushed = false
+        setupNavBar()
         setupView()
         setupConst()
         collectionViewConfigure()
         presenter.config()
+        presenter.getActors()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        title = "Trailer"
     }
     
     private func setupView() {
-        view.addSubviews(webView, titleView, stack)
+        view.addSubviews(webView, titleView, stack, header)
         stack.addArrangedSubview(descriptionTitle)
         stack.addArrangedSubview(descriptionTextView)
         view.backgroundColor = .background
         webView.backgroundColor = .clear
         webView.layer.cornerRadius = 15
         webView.clipsToBounds = true
-        
+    }
+    
+    private func setupNavBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: button)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: likeButton)
+        button.addTarget(self, action: #selector(backAction), for: .touchUpInside)
+        likeButton.addTarget(self, action: #selector(likeAction), for: .touchUpInside)
     }
     
     private func configView(with model: Doc) {
@@ -80,7 +116,13 @@ final class TrailerController: UIViewController {
             stack.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: 32),
             stack.leadingAnchor.constraint(equalTo: webView.leadingAnchor),
             stack.trailingAnchor.constraint(equalTo: webView.trailingAnchor),
-            stack.heightAnchor.constraint(equalToConstant: 174),
+            stack.heightAnchor.constraint(equalToConstant: 300),
+            
+            header.topAnchor.constraint(equalTo: stack.bottomAnchor, constant: 25),
+            header.leadingAnchor.constraint(equalTo: stack.leadingAnchor),
+            header.trailingAnchor.constraint(lessThanOrEqualTo: stack.trailingAnchor),
+            header.heightAnchor.constraint(equalToConstant: 20)
+            
             
         ])
     }
@@ -88,22 +130,38 @@ final class TrailerController: UIViewController {
     //MARK: - CollectionView configure
     private func collectionViewConfigure() {
         let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
+        layout.scrollDirection = .horizontal
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView?.backgroundColor = .red
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(ActorCell.self, forCellWithReuseIdentifier: ActorCell.identifier)
         view.addSubview(collectionView)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: stack.bottomAnchor, constant: 24),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.topAnchor.constraint(equalTo: header.bottomAnchor, constant: 20),
             collectionView.leadingAnchor.constraint(equalTo: webView.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: webView.trailingAnchor),
+            collectionView.heightAnchor.constraint(equalToConstant: 50)
         ])
+    }
+    
+    @objc private func backAction() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc private func likeAction() {
+        //MARK: - Добавление в вишлист
     }
 }
 
 extension TrailerController: TrailerViewProtocol {
+    func updateActors() {
+        collectionView.reloadData()
+    }
+    
     func update(with model: Doc, text: String) {
         titleView.config(with: model)
         descriptionTextView.text = model.description
@@ -115,30 +173,35 @@ extension TrailerController: TrailerViewProtocol {
     
 }
 
+extension TrailerController: UICollectionViewDataSource {
 
-//MARK: - SwiftUI
-import SwiftUI
-struct Provider_TrailerController : PreviewProvider {
-    static var previews: some View {
-        ContainterView().edgesIgnoringSafeArea(.all)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return presenter.filmPersons?.count ?? 0
     }
     
-    struct ContainterView: UIViewControllerRepresentable {
-        func makeUIViewController(context: Context) -> UIViewController {
-            return TrailerController()
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ActorCell.identifier, for: indexPath) as? ActorCell else { return UICollectionViewCell() }
+        if let actor = presenter.filmPersons?[indexPath.row] {
+            cell.config(with: actor)
         }
-        
-        typealias UIViewControllerType = UIViewController
-        
-        
-        let viewController = TrailerController()
-        func makeUIViewController(context: UIViewControllerRepresentableContext<Provider_TrailerController.ContainterView>) -> TrailerController {
-            return viewController
-        }
-        
-        func updateUIViewController(_ uiViewController: Provider_TrailerController.ContainterView.UIViewControllerType, context: UIViewControllerRepresentableContext<Provider_TrailerController.ContainterView>) {
-            
-        }
+        return cell
     }
+    
     
 }
+
+extension TrailerController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 45)
+    }
+    
+
+    
+}
+
+
+
+
+
+
+
