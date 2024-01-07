c02b74a49a9835407ab0a8747a49127480792b50
Nikita <86856995+SemennikovNA@users.noreply.github.com>
2024-01-07 15:08:31 +0600

    Nikita/fix UI profile (#36)
    * Added button image
    
    * Create choise image for user profile, added blue arrow for button in profile
    
    ---------
    
    Co-authored-by: Colokol <Colokol1@gmail.com>

--- Movie Today/Modules/ProfileScreen/EditProfileViewController.swift
+++ Movie Today/Modules/ProfileScreen/EditProfileViewController.swift
@@ -12,6 +12,7 @@ class EditProfileViewController: UIViewController {
     //MARK: - User interface elements
     
     let editProfileView = EditProfileView()
+    let userView = UserView()
     
     // MARK: - Life cycle
     
@@ -27,15 +28,66 @@ class EditProfileViewController: UIViewController {
     //MARK: - Private methods
     
     private func setupView() {
+        
+        // Setup view
         view.addSubviews(editProfileView)
+        
+        // Add target's
+        editProfileView.addTargetFofEditButton(target: self, selector: #selector(changeImage))
     }
 
     private func signatureDelegate() {
         editProfileView.signatureTextFieldDelegate()
     }
+    
+    @objc func changeImage() {
+        
+        let cameraIcon = UIImage(systemName: "camera")
+        let photoLibrary = UIImage(systemName: "photo")
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+            self.choiseImagePicker(.camera)
+        }
+        camera.setValue(cameraIcon, forKey: "image")
+        camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+        
+        let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+            self.choiseImagePicker(.photoLibrary)
+        }
+        
+        photo.setValue(photoLibrary, forKey: "image")
+        photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        actionSheet.addAction(camera)
+        actionSheet.addAction(photo)
+        actionSheet.addAction(cancel)
+        present(actionSheet, animated: true)
+    }
 }
 
 //MARK: - Extension
+
+extension EditProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func choiseImagePicker(_ sourse: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(sourse) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = sourse
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        editProfileView.userImage.image = info[.editedImage] as? UIImage
+        editProfileView.userImage.contentMode = .scaleAspectFit
+        editProfileView.userImage.clipsToBounds = true
+        dismiss(animated: true)
+    }
+}
 
 private extension EditProfileViewController {
     
