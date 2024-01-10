a4562beba9fae1bebff5eccd15bac4c53b145b84
KrasnovYuri <krasnov1yuri@gmail.com>
2024-01-10 13:38:55 +0300

    fix

--- Movie Today/Modules/NotificationScreen/NotificationViewController.swift
+++ Movie Today/Modules/NotificationScreen/NotificationViewController.swift
@@ -9,21 +9,89 @@ import UIKit
 
 class NotificationViewController: UIViewController {
 
+    
+    //MARK: - UI Elements
+    
+    private let messagesNotificationsLabel: UILabel = {
+        UILabel.createMetadataLabel(text: "Messages Notifications")
+    }()
+    
+    private let showNotificationsLabel: UILabel = {
+        UILabel(text: "Show Notifications", font: .systemFont(ofSize: 20), textColor: .white)
+    }()
+    
+    private let exceptionsButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Exceptions", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let switcher: UISwitch = {
+        let switcher = UISwitch()
+        switcher.onTintColor = .blueAccent
+        switcher.translatesAutoresizingMaskIntoConstraints = false
+        return switcher
+    }()
+    
+    private lazy var showNotificationsStack: UIStackView = {
+        createStack(arrangedSubviews: [showNotificationsLabel, switcher], axis: .horizontal)
+    }()
+    
+    private lazy var verticalStack: UIStackView = {
+        createStack(arrangedSubviews: [messagesNotificationsLabel, showNotificationsStack, exceptionsButton], axis: .vertical)
+    }()
+    
+    //MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupSwitcher()
+        setConstraints()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - Private Methods
+    
+    private func createStack(arrangedSubviews views: [UIView], axis: NSLayoutConstraint.Axis) -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: views)
+        stackView.axis = axis
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }
-    */
 
+    private func setupViews() {
+        view.backgroundColor = UIColor.background
+        view.addSubview(verticalStack)
+    }
+    
+    private func setupSwitcher() {
+        switcher.isOn = UserDefaults.standard.bool(forKey: "switchState")
+        switcher.addTarget(self, action: #selector(switchValueChanged), for: .valueChanged)
+    }
+    
+    //MARK: - Actions
+    
+    @objc private func switchValueChanged() {
+        UserDefaults.standard.set(switcher.isOn, forKey: "switchState")
+    }
+    
+    // MARK: - Set Constraints
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            exceptionsButton.widthAnchor.constraint(equalTo: exceptionsButton.titleLabel!.widthAnchor),
+            
+            verticalStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            verticalStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            verticalStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+            
+        ])
+    }
+    
 }
