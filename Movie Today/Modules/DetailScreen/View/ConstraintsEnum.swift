b6bc70af9eaab94911a6bd700473ae1c72815a38
Timofey Spodeneyko <45203444+TSpodeneyko@users.noreply.github.com>
2024-01-10 09:17:03 +0300

    Finished DetailScreen (#56)

--- Movie Today/Modules/DetailScreen/View/ConstraintsEnum.swift
+++ Movie Today/Modules/DetailScreen/View/ConstraintsEnum.swift
@@ -9,22 +9,28 @@ import UIKit
 
 enum LayoutConstants {
     static let topMargin: CGFloat = 20
+    static let smallTopMargin: CGFloat = 5
     static let leadingMargin: CGFloat = 15
     static let trailingMargin: CGFloat = -15
-    static let headerHeight: CGFloat = 30
-    static let movieImageWidth: CGFloat = 200
-    static let movieImageHeightMultiplier: CGFloat = 16.0/9.0
-    static let metadataStackViewLeadingMargin: CGFloat = 50
-    static let metadataStackViewTrailingMargin: CGFloat = -50
+    static let descriptionTitleLeadingMargin: CGFloat = 20
+    static let personInfoStackViewLeadingMargin: CGFloat = 5
+    static let personInfoStackViewTrailingMargin: CGFloat = -5
+    static let movieImageWidth: CGFloat = 350
+    static let movieImageHeight: CGFloat = 450
     static let metadataStackViewHeight: CGFloat = 24
     static let ratingStackViewHeight: CGFloat = 20
-    static let buttonHeight: CGFloat = 50
-    static let buttonWidth: CGFloat = 140
+    static let buttonHeight: CGFloat = 40
+    static let trailerButtonWidth: CGFloat = 120
     static let buttonsSpacing: CGFloat = 20
-    static let buttonsLeadingMargin: CGFloat = 70
-    static let buttonsTrailingMargin: CGFloat = -70
     static let contentPadding: CGFloat = 20
     static let movieBackgroundViewBottomOffset: CGFloat = 150
-    static let dividerWidth: CGFloat = 1
     static let dividerHeight: CGFloat = 24
+    static let dividerWidth: CGFloat = 1
+    static let personImageSize: CGFloat = 80
+    static let actorsCollectionViewCellWidth: CGFloat = 200
+    
+    static let gradientLayerXStartPoint = 0.5
+    static let gradientLayerYStartPoint = 0.5
+    static let gradientLayerXEndPoint = 0.5
+    static let gradientLayerYEndPoint = 1.0
 }
