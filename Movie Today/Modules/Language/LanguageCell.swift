bc815bfb0d7738fe26162da3f54f0bf312cf62bc
KrasnovYuri <krasnov1yuri@gmail.com>
2024-01-11 14:03:25 +0300

    fix

--- Movie Today/Modules/Language/LanguageCell.swift
+++ Movie Today/Modules/Language/LanguageCell.swift
@@ -9,15 +9,58 @@ import UIKit
 
 class LanguageCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override var isSelected: Bool {
+        didSet {
+            selectedLanguageLabel.isHidden = !isSelected
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    // MARK: - UI Elements
+    
+    private let languageLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        return label
+    }()
+    
+    private let selectedLanguageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "✓"
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .blueAccent
+        return label
+    }()
+    
+    // MARK: - View Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+   
+    // MARK: - Methods
+    
+    func configure(language: Language) {
+        languageLabel.text = language.name
+    }
+    
+    private func setupCell() {
+        self.backgroundColor = .background
+        [languageLabel, selectedLanguageLabel].forEach { $0.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            languageLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            languageLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 30),
+            languageLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20),
+            
+            selectedLanguageLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            selectedLanguageLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -30),
+            selectedLanguageLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20)
+        ])
+    }
 }
--- Movie Today/Modules/Language/LanguageTableViewController.swift
+++ Movie Today/Modules/Language/LanguageTableViewController.swift
@@ -8,22 +8,118 @@
 import UIKit
 
 class LanguageTableViewController: UIViewController {
-
+    
+    // MARK: - Private Properties
+    
+    private var selectedIndexPath: IndexPath?
+    private let selectedLanguageKey = "SelectedLanguage"
+    private var languages = Source.makeLanguageWithGroup()
+    private let tableView: UITableView = .init(frame: .zero, style: .insetGrouped)
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTableView()
+        
+        tableView.register(LanguageCell.self, forCellReuseIdentifier: "LanguageCell")
+        tableView.backgroundColor = .background
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        configureSelectedIndexPath()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Methods
+    
+    func configureSelectedIndexPath() {
+        if let selectedLanguageName = UserDefaults.standard.string(forKey: selectedLanguageKey) {
+            for (sectionIndex, section) in languages.enumerated() {
+                for (rowIndex, language) in section.enumerated() {
+                    if language.name == selectedLanguageName {
+                        selectedIndexPath = IndexPath(row: rowIndex, section: sectionIndex)
+                        break
+                    }
+                }
+                if selectedIndexPath != nil {
+                    break
+                }
+            }
+        }
     }
-    */
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+// MARK: - UITableViewDataSource
 
+extension LanguageTableViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0: "Suggested Languages"
+        case 1: "Other Language"
+        default: nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        languages[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LanguageCell", for: indexPath) as! LanguageCell
+        cell.selectionStyle = .none
+        
+        if let selectedIndexPath = selectedIndexPath {
+            cell.isSelected = (indexPath == selectedIndexPath)
+        } else {
+            cell.isSelected = false
+        }
+        
+        cell.configure(language: languages[indexPath.section][indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        
+        let header = view as! UITableViewHeaderFooterView
+        header.textLabel?.textColor = .customGray
+        header.textLabel?.font = UIFont.montserratSemiBold(ofSize: 14)
+    }
+    
+    
+}
+
+// MARK: - UITableViewDelegate
+
+extension LanguageTableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) as? LanguageCell {
+            cell.isSelected = true
+            selectedIndexPath = indexPath
+            
+            let selectedLanguage = languages[indexPath.section][indexPath.row]
+            UserDefaults.standard.set(selectedLanguage.name, forKey: selectedLanguageKey)
+        }
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.layer.borderColor = UIColor.darkGray.cgColor
+        cell.layer.borderWidth = 1.0
+    }
 }
